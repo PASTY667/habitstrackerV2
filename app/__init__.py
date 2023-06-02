1e74fa2594467b2f8cfe469b9437c325d67d69c3
PASTY667 <105501129+PASTY667@users.noreply.github.com>
2023-06-02 20:24:14 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,13 @@
 from app import app
+from flask_sqlalchemy import SQLAlchemy
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'  # Exemple d'URI de base de données
+db = SQLAlchemy(app)
+
  

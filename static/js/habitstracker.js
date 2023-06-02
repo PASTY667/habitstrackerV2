44f6495207f5746d55ec191cc9721bc667492934
PASTY667 <105501129+PASTY667@users.noreply.github.com>
2023-06-02 20:08:17 +0200

    Update habitstracker.js
    Created the base of the code

--- static/js/habitstracker.js
+++ static/js/habitstracker.js
@@ -1,1 +1,31 @@
- 
+ document.getElementById('createHabitForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  var habitName = document.getElementById('habitName').value;
+
+  fetch('{{ url_for('create_habit') }}', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: 'habit_name=' + encodeURIComponent(habitName)
+  })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    if (data.success) {
+      var habitList = document.getElementById('habitList');
+      var newHabitItem = document.createElement('li');
+      newHabitItem.textContent = habitName;
+      habitList.appendChild(newHabitItem);
+      document.getElementById('habitName').value = '';
+    } else {
+      console.log('Erreur lors de la création de l\'habitude :', data.error);
+    }
+  })
+  .catch(function(error) {
+    console.log('Erreur :', error);
+  });
+});
+

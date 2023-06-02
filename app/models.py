a6f8664aff1709725d889b9accde5310c018ce33
PASTY667 <105501129+PASTY667@users.noreply.github.com>
2023-06-02 21:11:49 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,10 +1,18 @@
- from app import db
+from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
     def __repr__(self):
-        return f"User(id={self.id}, username='{self.username}')"
+        return '<User %r>' % self.username
+
+class Habit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return '<Habit %r>' % self.name
+
 
